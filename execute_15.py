4f34d582080866eaebfbfe67997252bf6ba09773
Kritsakorn <mintchild@outlook.com>
2023-02-28 16:14:06 +0700

    Fix query and some bugs

--- execute_15.py
+++ execute_15.py
@@ -95,7 +95,13 @@ connection_url = URL.create(
         "TrustServerCertificate": "yes",
     },
 )
-mssql_engine = create_engine(connection_url)
+try:
+    mssql_engine = create_engine(connection_url)
+    print("Sandbox2 Server Conected.")
+except exc.SQLAlchemyError as e:
+    error = str(e)
+    print(error)
+# mssql_engine = create_engine(connection_url)
 
 sql1 = '''
 SELECT tb1.*, tb2.date_time FROM [dbo].[tb_pea_customer] tb1
@@ -106,7 +112,7 @@ WHERE tb1.meter_point_id IS NOT NULL;'''
 # for i in cursor:
 #     print(i)
 with mssql_engine.connect() as conn:
-    df = pd.read_sql_query(sql=text(sql1), con=mssql_engine)
+    df = pd.read_sql_query(sql=text(sql1), con=conn)
 # medter_dict = df.to_dict()
 # print(medter_dict)
 print(df)
@@ -150,19 +156,19 @@ try:
             "events": True
         }
     )
-    print("Conected.")
+    print("AMR Conected.")
 except exc.SQLAlchemyError as e:
     error = str(e)
     print(error)
 
 
-def getandsave(meterpointlist_str, meter, date_end):
+def getandsave(meter, date_end):
     sql_cus = '''
     SELECT mp.meterpointid, cus.customercode
     FROM edmi.tblmeterpoints mp
     LEFT JOIN edmi.tblcustomers cus ON mp.customerid = cus.customerid
-    WHERE mp.meterpointid IN ({meterpointlist})
-    '''.format(meterpointlist=meterpointlist_str)
+    WHERE mp.meterpointid = :mtpid
+    '''
 
     sql_whimp = '''
     SELECT
@@ -173,8 +179,8 @@ def getandsave(meterpointlist_str, meter, date_end):
         edmi.tblprofilewhimp whimp
     WHERE
         whimp.date_m BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
-        AND whimp.meterpointid IN ({meterpointlist})
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        AND whimp.meterpointid = :mtpid
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_varimp = '''
     SELECT
@@ -185,8 +191,8 @@ def getandsave(meterpointlist_str, meter, date_end):
         edmi.tblprofilevarhimp varimp
     WHERE
         varimp.date_m BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
-        AND varimp.meterpointid IN ({meterpointlist})
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        AND varimp.meterpointid = :mtpid
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_whexp = '''
     SELECT
@@ -197,8 +203,8 @@ def getandsave(meterpointlist_str, meter, date_end):
         edmi.tblprofilewhexp whexp
     WHERE
         whexp.date_m BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
-        AND whexp.meterpointid IN ({meterpointlist})
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        AND whexp.meterpointid = :mtpid
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_varexp = '''
     SELECT
@@ -209,8 +215,8 @@ def getandsave(meterpointlist_str, meter, date_end):
         edmi.tblprofilevarhexp varexp
     WHERE
         varexp.date_m BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
-        AND varexp.meterpointid IN ({meterpointlist})
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        AND varexp.meterpointid = :mtpid
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_pf = '''
     SELECT
@@ -222,8 +228,8 @@ def getandsave(meterpointlist_str, meter, date_end):
     WHERE
         pf.date_m BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
         AND pf.phase = 3
-        AND pf.meterpointid IN ({meterpointlist})
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        AND pf.meterpointid = :mtpid
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_insti = '''
     SELECT
@@ -241,7 +247,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinsti
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
             AND PHASE = 0
     ) IA
@@ -253,7 +259,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinsti
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 1) IB ON
         ia.meterpointid = ib.meterpointid
@@ -266,12 +272,12 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinsti
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 2) IC ON
         ia.meterpointid = ic.meterpointid
         AND ia.date_m = ic.date_m
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_instv = '''
     SELECT
@@ -289,7 +295,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinstv
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
             AND PHASE = 0
     ) VA
@@ -301,7 +307,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinstv
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 1) VB ON
         va.meterpointid = vb.meterpointid
@@ -314,12 +320,12 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinstv
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 2) VC ON
         va.meterpointid = vc.meterpointid
         AND va.date_m = vc.date_m
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_instang = '''
     SELECT
@@ -337,7 +343,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinstang
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
             AND PHASE = 0
     ) ANGA
@@ -349,7 +355,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinstang
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 1) ANGB ON
         anga.meterpointid = angb.meterpointid
@@ -362,12 +368,12 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofileinstang
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 2) ANGC ON
         anga.meterpointid = angc.meterpointid
         AND anga.date_m = angc.date_m
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_thdi = '''
     SELECT
@@ -385,7 +391,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofilethdi
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
             AND PHASE = 0
     ) THDIA
@@ -397,7 +403,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofilethdi
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 1) THDIB ON
         thdia.meterpointid = thdib.meterpointid
@@ -410,12 +416,12 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofilethdi
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 2) THDIC ON
         thdia.meterpointid = thdic.meterpointid
         AND thdia.date_m = thdic.date_m
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
 
     sql_thdv = '''
     SELECT
@@ -433,7 +439,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofilethdv
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
             AND PHASE = 0
     ) THDVA
@@ -445,7 +451,7 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofilethdv
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 1) THDVB ON
         thdva.meterpointid = thdvb.meterpointid
@@ -458,49 +464,50 @@ def getandsave(meterpointlist_str, meter, date_end):
         FROM
             edmi.tblprofilethdv
         WHERE
-            METERPOINTID IN ({meterpointlist})
+            METERPOINTID = :mtpid
             AND DATE_M BETWEEN TO_DATE(\'{datetime_start}\','YYYY-MM-DD HH24.MI') AND TO_DATE(\'{datetime_end}\','YYYY-MM-DD HH24.MI')
                 AND PHASE = 2) THDVC ON
         thdva.meterpointid = thdvc.meterpointid
         AND thdva.date_m = thdvc.date_m
-        '''.format(datetime_start=dt_start, datetime_end=date_end,meterpointlist=meterpointlist_str)
-    df_cus = pd.read_sql_query(sql_cus, engine, params=meter)
-    print(df_cus)
-    df_whimp = pd.read_sql_query(sql_whimp, engine, params=meter)
-    print(df_whimp)
-    df_varhimp = pd.read_sql_query(sql_varimp, engine, params=meter)
-    print(df_varhimp)
-    df_whexp = pd.read_sql_query(sql_whexp, engine, params=meter)
-    print(df_whexp)
-    df_varhexp = pd.read_sql_query(sql_varexp, engine, params=meter)
-    print(df_varhexp)
-    df_pf = pd.read_sql_query(sql_pf, engine, params=meter)
-    print(df_pf)
-    df_instv = pd.read_sql_query(sql_instv, engine, params=meter)
-    print(df_instv)
-    df_insti = pd.read_sql_query(sql_insti, engine, params=meter)
-    print(df_insti)
-    df_ang = pd.read_sql_query(sql_instang, engine, params=meter)
-    print(df_ang)
-    df_thdi = pd.read_sql_query(sql_thdi, engine, params=meter)
-    # print(df_thdi)
-    # df_thdv = pd.read_sql_query(sql_thdv, engine, params=meterpointlist)
-    # print(df_thdv)
-    df_wh = pd.merge(df_whimp, df_whexp, how="left", on=["meterpointid", "datetime_meter"],suffixes=["_whimp","_whexp"])
-    print(df_wh)
-    df_varh = pd.merge(df_varhimp, df_varhexp, how="left", on=["meterpointid", "datetime_meter"])
-    print(df_varh)
-    df_all = pd.merge(df_wh, df_instv, how="left", on=["meterpointid", "datetime_meter"])
-    df_1 = pd.merge(df_insti, df_instv, how="left", on=["meterpointid", "datetime_meter"])
-    df_2 = pd.merge(df_ang,df_varh,how="left", on=["meterpointid", "datetime_meter"])
-    df_3 = pd.merge(df_1,df_2,how="left", on=["meterpointid", "datetime_meter"])
-    df_4 = pd.merge(df_3,df_pf,how="left", on=["meterpointid", "datetime_meter"])
-    df_5 = pd.merge(df_4, df_cus, how="left", on=["meterpointid"])
-    df_all = pd.merge(df_5,df_wh,how="left", on=["meterpointid", "datetime_meter"])
-    # df_all.to_csv("test.csv",index=False)
-    df_final = df_all.replace(({np.NaN:None}))
-    print(df_final)
-    # df_final.info()
+        '''.format(datetime_start=dt_start, datetime_end=date_end)
+    with engine.connect() as orac_conn:
+        df_cus = pd.read_sql_query(sql=text(sql_cus), con=orac_conn, params={"mtpid":meter})
+        print(df_cus)
+        df_whimp = pd.read_sql_query(sql=text(sql_whimp), con=orac_conn, params={"mtpid":meter})
+        print(df_whimp)
+        df_varhimp = pd.read_sql_query(sql=text(sql_varimp), con=orac_conn, params={"mtpid":meter})
+        print(df_varhimp)
+        df_whexp = pd.read_sql_query(sql=text(sql_whexp), con=orac_conn, params={"mtpid":meter})
+        print(df_whexp)
+        df_varhexp = pd.read_sql_query(sql=text(sql_varexp), con=orac_conn, params={"mtpid":meter})
+        print(df_varhexp)
+        df_pf = pd.read_sql_query(sql=text(sql_pf), con=orac_conn, params={"mtpid":meter})
+        print(df_pf)
+        df_instv = pd.read_sql_query(sql=text(sql_instv), con=orac_conn, params={"mtpid":meter})
+        print(df_instv)
+        df_insti = pd.read_sql_query(sql=text(sql_insti), con=orac_conn, params={"mtpid":meter})
+        print(df_insti)
+        df_ang = pd.read_sql_query(sql=text(sql_instang), con=orac_conn, params={"mtpid":meter})
+        print(df_ang)
+        df_thdi = pd.read_sql_query(sql=text(sql_thdi), con=orac_conn, params={"mtpid":meter})
+        # print(df_thdi)
+        # df_thdv = pd.read_sql_query(sql=text(sql_thdv), con=orac_conn, params={"mtpid":meter}pointlist)
+        # print(df_thdv)
+        df_wh = pd.merge(df_whimp, df_whexp, how="left", on=["meterpointid", "datetime_meter"],suffixes=["_whimp","_whexp"])
+        print(df_wh)
+        df_varh = pd.merge(df_varhimp, df_varhexp, how="left", on=["meterpointid", "datetime_meter"])
+        print(df_varh)
+        df_all = pd.merge(df_wh, df_instv, how="left", on=["meterpointid", "datetime_meter"])
+        df_1 = pd.merge(df_insti, df_instv, how="left", on=["meterpointid", "datetime_meter"])
+        df_2 = pd.merge(df_ang,df_varh,how="left", on=["meterpointid", "datetime_meter"])
+        df_3 = pd.merge(df_1,df_2,how="left", on=["meterpointid", "datetime_meter"])
+        df_4 = pd.merge(df_3,df_pf,how="left", on=["meterpointid", "datetime_meter"])
+        df_5 = pd.merge(df_4, df_cus, how="left", on=["meterpointid"])
+        df_all = pd.merge(df_5,df_wh,how="left", on=["meterpointid", "datetime_meter"])
+        # df_all.to_csv("test.csv",index=False)
+        df_final = df_all.replace(({np.NaN:None}))
+        print(df_final)
+        # df_final.info()
     #INSERT DATA TO MSSQL
     sql_update = '''
     MERGE [dbo].[tb_last_update] as t
@@ -518,6 +525,7 @@ def getandsave(meterpointlist_str, meter, date_end):
     '''
     for index, row in df_final.iterrows():
         print("insert row ", index, "Values = ",row.meterpointid, row.tae_i_a, row.tae_i_b, row.tae_i_c, row.tae_v_a, row.tae_v_b, row.tae_v_c, row.tae_pf, row.tae_ang_a, row.tae_ang_b, row.tae_ang_c, row.tae_kvarh_imp, row.tae_kvarh_exp, row.tae_kwh_imp, row.tae_kwh_exp, row.datetime_meter)
+        
         cursor.execute(sql_insert, row.meterpointid, row.tae_i_a, row.tae_i_b, row.tae_i_c, row.tae_v_a, row.tae_v_b, row.tae_v_c, row.tae_pf, row.tae_ang_a, row.tae_ang_b, row.tae_ang_c, row.tae_kvarh_imp, row.tae_kvarh_exp, row.tae_kwh_imp, row.tae_kwh_exp, row.datetime_meter, row.customercode)
         cursor.execute(sql_update, row.meterpointid, row.datetime_meter)
     cnxn.commit()
@@ -525,37 +533,37 @@ def getandsave(meterpointlist_str, meter, date_end):
 
 #query on by one
 for device in meterpointlist:
-    device = [device]
-    in_vars = ','.join(':%d' % i for i in range(len(device)))
+    # device = [device]
+    # in_vars = ','.join(':%d' % i for i in range(len(device)))
     # print(in_vars) 
-    if dictionary[device[0]]:
-        print("Value from list is " + str(device[0]))
-        print("Value from dictionary is " + str(dictionary[device[0]]))
+    if dictionary[device]:
+        print("Value from list is " + str(device))
+        print("Value from dictionary is " + str(dictionary[device]))
         #Get last update + 15 min to check if it equal to current query time
-        lastest_ = datetime.strptime(dictionary[device[0]],'%Y-%m-%d %H:%M')
-        log_msg = "Latest update of meterpointid "+ str(device[0]) + " = " + lastest_.strftime('%Y-%m-%d %H:%M')
+        lastest_ = datetime.strptime(dictionary[device],'%Y-%m-%d %H:%M')
+        log_msg = "Latest update of meterpointid "+ str(device) + " = " + lastest_.strftime('%Y-%m-%d %H:%M')
         print(log_msg)
         logging.info(log_msg)
         lastest = lastest_ + timedelta(minutes=15)
         print("Time for checking is " + lastest.strftime('%Y-%m-%d %H:%M'))
         if querytime == lastest:
-            log_msg = "Time using for query of meterpointid " + str(device[0]) + " is " + querytime.strftime('%Y-%m-%d %H:%M')
+            log_msg = "Time using for query of meterpointid " + str(device) + " is " + querytime.strftime('%Y-%m-%d %H:%M')
             logging.info(log_msg)
             print("log_msg")
             dt_end = dt_start = querytime.strftime('%Y-%m-%d %H:%M')
         else:
 
-            log_msg = "Time using for query of meterpointid " + str(device[0]) + " is " + lastest.strftime('%Y-%m-%d %H:%M') + " to " + querytime.strftime('%Y-%m-%d %H:%M')
+            log_msg = "Time using for query of meterpointid " + str(device) + " is " + lastest.strftime('%Y-%m-%d %H:%M') + " to " + querytime.strftime('%Y-%m-%d %H:%M')
             logging.info(log_msg)
             print("log_msg")
             dt_start = lastest.strftime('%Y-%m-%d %H:%M')
             dt_end = querytime.strftime("%Y-%m-%d %H:%M")
     else:
         print("No latest time is found, maybe a new meter?")
-        log_msg = "Using default start time for query of new meterpointid " + str(device[0]) + " = as " + default_query_time.strftime("%Y-%m-%d %H:%M")
+        log_msg = "Using default start time for query of new meterpointid " + str(device) + " = as " + default_query_time.strftime("%Y-%m-%d %H:%M")
         logging.info(log_msg)
         print("log_msg")
         dt_start = default_query_time.strftime("%Y-%m-%d %H:%M")
         dt_end = querytime.strftime("%Y-%m-%d %H:%M")
-    getandsave(in_vars, device, dt_end)
+    getandsave(device, dt_end)
 cursor.close()
